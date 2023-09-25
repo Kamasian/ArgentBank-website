9448a0e0b766b24dd04bf6f5b3632817e506fc10
Dugrais Emmanuel <emmanuel.dugrais@gmail.com>
2023-09-25 19:58:31 +0200

    Création de Home, Header. 1ere route crée, et ajout du lien Font Awesome

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,7 +1,14 @@
-import './App.css'
+import { Routes, Route } from "react-router-dom"
+import Header from "./components/Header/Header"
+import Home from "./pages/Home/Home"
 
 export default function App() {
   	return (
-     <div></div>
+		<div>
+				<Header />
+				<Routes>
+                	<Route path="/" element={<Home />} />
+				</Routes>
+	 	</div>
   	)
 }
